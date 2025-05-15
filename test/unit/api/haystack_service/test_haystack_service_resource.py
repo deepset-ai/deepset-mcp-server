bada6ea10eb8170a6c1d18560b7b35d96b049c9d
mathislucka <mathis.lucka@gmail.com>
2025-05-15 11:47:49 +0200

    refactor: update test to match new params approach

--- test/unit/api/haystack_service/test_haystack_service_resource.py
+++ test/unit/api/haystack_service/test_haystack_service_resource.py
@@ -40,7 +40,7 @@ def mock_schema_error_response(mock_client: BaseFakeClient) -> None:
 
 @pytest.fixture
 def mock_successful_io_response(mock_client: BaseFakeClient) -> None:
-    mock_client.responses["v1/haystack/components/input-output?domain=deepset-cloud&names=Agent"] = TransportResponse(
+    mock_client.responses["v1/haystack/components/input-output"] = TransportResponse(
         status_code=200,
         json=[{"name": "Agent", "input": "Mock input", "output": "Mock output"}],
         text=json.dumps([{"name": "Agent", "input": "Mock input", "output": "Mock output"}]),
