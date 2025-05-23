25ce09cb8c7daad216a0920b3fa9e68693703db9
mathislucka <mathis.lucka@gmail.com>
2025-05-23 12:27:50 +0200

    feat: import PipelineHandle in resource module

--- src/deepset_mcp/api/pipeline/resource.py
+++ src/deepset_mcp/api/pipeline/resource.py
@@ -8,6 +8,7 @@ from deepset_mcp.api.pipeline.models import (
     ValidationError,
 )
 from deepset_mcp.api.protocols import AsyncClientProtocol, PipelineResourceProtocol
+from deepset_mcp.api.pipeline.handle import PipelineHandle
 from deepset_mcp.api.transport import raise_for_status
 
 
