ba89325138ea8261503b1fadcfdccd2135b1ab08
mathislucka <mathis.lucka@gmail.com>
2025-05-23 13:32:53 +0200

    refactor: update list test to expect DeepsetPipeline instead of handle

--- test/unit/api/pipeline/test_pipeline_resource.py
+++ test/unit/api/pipeline/test_pipeline_resource.py
@@ -83,10 +83,9 @@ class TestPipelineResource:
 
         # Verify results
         assert len(result) == 2
-        assert isinstance(result[0], PipelineHandle)
+        assert isinstance(result[0], DeepsetPipeline)
         assert result[0].id == "1"
         assert result[0].name == "Pipeline 1"
-        assert isinstance(result[0].pipeline, DeepsetPipeline)
 
         # Verify request
         assert len(client.requests) == 1
