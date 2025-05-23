26eb3e55439bc6ea6eccc6923072885da5591790
mathislucka <mathis.lucka@gmail.com>
2025-05-23 13:33:48 +0200

    refactor: remove PipelineHandle from pipeline package __init__.py

--- src/deepset_mcp/api/pipeline/__init__.py
+++ src/deepset_mcp/api/pipeline/__init__.py
@@ -1,10 +1,8 @@
-from .handle import PipelineHandle
 from .log_models import PipelineLog, PipelineLogList
 from .models import DeepsetPipeline, NoContentResponse, PipelineValidationResult, ValidationError
 from .resource import PipelineResource
 
 __all__ = [
-    "PipelineHandle",
     "DeepsetPipeline",
     "NoContentResponse",
     "PipelineValidationResult",
