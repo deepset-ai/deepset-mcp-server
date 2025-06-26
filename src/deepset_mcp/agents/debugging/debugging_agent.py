e10b529c1746a56946eb254d6c3535852312def0
mathislucka <mathis.lucka@gmail.com>
2025-06-26 17:10:47 +0200

    feat: add agent for debugging (#121)
    * WIP: debugging agent
    
    * feat: add debugging agent

---
+++ src/deepset_mcp/agents/debugging/debugging_agent.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+from haystack.components.agents.agent import Agent
+from haystack.utils.auth import Secret
+from haystack_integrations.components.generators.anthropic.chat.chat_generator import AnthropicChatGenerator
+from haystack_integrations.tools.mcp import MCPToolset, StdioServerInfo
+
+from deepset_mcp.benchmark.runner.config import BenchmarkConfig
+
+
+def get_agent(benchmark_config: BenchmarkConfig) -> Agent:
+    """Get an instance of the Generalist agent."""
+    tools = MCPToolset(
+        server_info=StdioServerInfo(
+            command="uv",
+            args=["run", "deepset-mcp"],
+            env={
+                "DEEPSET_WORKSPACE": benchmark_config.deepset_workspace,
+                "DEEPSET_API_KEY": benchmark_config.deepset_api_key,
+                "DEEPSET_DOCS_API_KEY": benchmark_config.get_env_var("DEEPSET_DOCS_API_KEY"),
+                "DEEPSET_DOCS_WORKSPACE": benchmark_config.get_env_var("DEEPSET_DOCS_WORKSPACE"),
+                "DEEPSET_DOCS_PIPELINE_NAME": benchmark_config.get_env_var("DEEPSET_DOCS_PIPELINE_NAME"),
+            },
+        )
+    )
+    prompt = (Path(__file__).parent / "system_prompt.md").read_text()
+    generator = AnthropicChatGenerator(
+        model="claude-sonnet-4-20250514",
+        generation_kwargs={"max_tokens": 8000},
+        api_key=Secret.from_token(benchmark_config.get_env_var("ANTHROPIC_API_KEY")),
+    )
+
+    return Agent(tools=tools, system_prompt=prompt, chat_generator=generator)
